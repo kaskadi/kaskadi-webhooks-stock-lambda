732932a4887ce97d334a4016fbf8387b9a65ef01
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-02-20 15:15:32 +0100

    Logging into ysws-logs with type field

--- kaskadi-webhooks-stock-lambda.js
+++ kaskadi-webhooks-stock-lambda.js
@@ -35,8 +35,9 @@ function esLog(eventBody) {
   const logDocId = `${timestampHex[0]}-${timestampHex.substr(1, 5)}-${timestampHex.substr(5, 5)}`
   es.index({
     id: logDocId,
-    index: 'stock-webhook-log',
+    index: 'ysws-logs',
     body: {
+      type: 'stock',
       date: timestamp,
       ...eventBody
     }
